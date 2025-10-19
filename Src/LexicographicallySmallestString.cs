d2d9ad68d43a258006cfc2dcb299878251a9b427
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-19 09:21:18 +0200

    complete day 19-Oct-2025

---
+++ Src/LexicographicallySmallestString.cs
@@ -0,0 +1,57 @@
+﻿namespace Src
+{
+    public class LexicographicallySmallestString
+    {
+        public static string FindLexSmallestString(string s, int a, int b)
+        {
+            var best = s;
+            var seen = new HashSet<string> { s };
+            var q = new Queue<string>();
+            q.Enqueue(s);
+
+            while (q.Count > 0)
+            {
+                var cur = q.Dequeue();
+                if (IsLexicographicallySmaller(cur, best))
+                   best = cur;
+
+                var aNext = ApplyOperationA(cur, a);
+                if (seen.Add(aNext))
+                {
+                    q.Enqueue(aNext);
+                }
+
+                var bNext = ApplyOperationB(cur, b);
+                if (seen.Add(bNext))
+                {
+                    q.Enqueue(bNext);
+                }
+            }
+
+            return best;
+        }
+
+        public static string ApplyOperationA(string s, int a)
+        {
+            var chars = s.ToCharArray();
+
+            for (var i = 1; i < chars.Length ; i+=2)
+            {
+                var digit = chars[i] - '0';
+                var number = digit + a;
+
+                chars[i] = (char)(number % 10 + '0');
+            }
+
+            return new string(chars);
+        }
+
+        public static string ApplyOperationB(string s, int b)
+        {
+            var n = s.Length;
+            return s.Substring(n - b, b) + s.Substring(0, n - b);
+
+        }
+        public static bool IsLexicographicallySmaller(string a, string b) => string.CompareOrdinal(a, b) < 0;
+    }
+}
---
+++ Tests/LexicographicallySmallestStringTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Src;
+
+namespace Tests
+{
+    public class LexicographicallySmallestStringTests
+    {
+        [Theory]
+        [InlineData("3456", 5, "3951")]
+        [InlineData("2555", 9, "2454")]
+        [InlineData("47", 5, "42")]
+        public void Test1(string s, int a, string expected)
+        {
+            // Act
+            var result = LexicographicallySmallestString.ApplyOperationA(s, a);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("3456", 1,"6345")]
+        [InlineData("1234567", 3, "5671234")]
+        [InlineData("489", 2, "894")]
+        public void ApplyOperationB(string s, int b, string expected)
+        {
+            // Act
+            var result = LexicographicallySmallestString.ApplyOperationB(s, b);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("0158", "0190", true)]
+        [InlineData("0158", "0158", false)]
+        [InlineData("6547777", "1547777", false)]
+        public void IsLexicographicallySmaller(string a, string b, bool expected)
+        {
+            // Act
+            var result = LexicographicallySmallestString.IsLexicographicallySmaller(a, b);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("5525", 9, 2, "2050")]
+        [InlineData("74", 5, 1, "24")]
+        [InlineData("0011", 4, 2, "0011")]
+        public void FindLexSmallestString(string s, int a, int b, string expected)
+        {
+            // Act
+            var result = LexicographicallySmallestString.FindLexSmallestString(s, a, b);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
