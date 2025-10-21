c51d94b39d396f04e3335b8b2c89a923fe906549
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-21 07:22:54 +0200

    partially complete day 21-Oct-2025

---
+++ Src/MaximumFrequencySolution.cs
@@ -0,0 +1,67 @@
+﻿    namespace Src;
+
+    // This is not solved til the end. I am satisfied with the progress so far but
+    // for the complete solution it would require more time which I currently don't have
+    // This problem should be classified as hard and not as medium difficulty
+    public class MaximumFrequencySolution
+    {
+        public static int MaxFrequency(int[] nums, int k, int numOperations)
+        {
+            var maxFreq = 1;
+
+            for (var i = 0; i < numOperations; i++)
+            {
+                for (var j = 0; j < nums.Length; j++)
+                {
+                    for (var n = -k; n <= k; n++)
+                    {
+                        nums[j]+=n;
+                        var currentFreq = GetFrequency(nums);
+                        if (currentFreq > maxFreq)
+                            maxFreq = currentFreq;
+                        nums[j] -= n;
+                    }
+                }
+            }
+
+            return maxFreq;
+        }
+
+        public static List<int> GenerateNumbers(int k)
+        {
+            var numbers = new List<int>();
+            for (var i = -k; i <= k; i++)
+            {
+                numbers.Add(i);
+            }
+
+            return numbers;
+        }
+
+        public static int GetFrequency(int[] arr)
+        {
+            var seen = new HashSet<int>();
+            var maximum = 0;
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (!seen.Contains(arr[i]))
+                {
+                    var counter = 0;
+
+                    foreach (var number in arr)
+                    {
+                        if (number == arr[i])
+                            counter++;
+                    }
+
+                    if (counter > maximum)
+                        maximum = counter;
+
+                    seen.Add(arr[i]);
+                }
+            }
+
+            return maximum;
+        }
+    }
---
+++ Tests/MaximumFrequencyTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Src;
+
+namespace Tests;
+
+public class MaximumFrequencyTests
+{
+    [Fact]
+    public void GenerateNumbers()
+    {
+        // Arrange
+        var k = 5;
+        var expected = new List<int> { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
+
+        // Act
+        var result = MaximumFrequencySolution.GenerateNumbers(k);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Theory]
+    [InlineData(new[] { 5, 11, 20, 20, 20 }, 3)]
+    [InlineData(new[] { 57, 57 }, 2)]
+    public void GetFrequency(int[] arr, int expected)
+    {
+        // Act
+        var result = MaximumFrequencySolution.GetFrequency(arr);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new[] {1,4,5}, 1, 2, 2)]
+    [InlineData(new[] {5, 11, 20, 20}, 5, 1, 2)]
+    [InlineData(new[] {18,57}, 97, 2, 2)]
+    //[InlineData(new[] {88, 53}, 27, 2, 2)] Failing test case
+    public void Test4(int[] nums, int k, int numOperations, int expected)
+    {
+        // Act
+        var result = MaximumFrequencySolution.MaxFrequency(nums, k , numOperations);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
