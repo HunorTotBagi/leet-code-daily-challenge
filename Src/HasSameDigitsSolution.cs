b5f0d95d18ceb1c4004299bbca7749287c193e6d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-23 05:40:01 +0200

    complete day 23-Oct-2025

---
+++ Src/HasSameDigitsSolution.cs
@@ -0,0 +1,31 @@
+﻿namespace Src;
+
+public class HasSameDigitsSolution
+{
+    public static bool HasSameDigits(string s)
+    {
+        var numbers = new List<int>();
+
+        foreach (var c in s)
+        {
+            numbers.Add(c - '0');
+        }
+
+        while (numbers.Count > 2)
+        {
+            var newNumbers = new List<int>();
+            for (var i = 0; i < numbers.Count-1; i++)
+            {
+                var number = (numbers[i] + numbers[i + 1]) % 10;
+                newNumbers.Add(number);
+            }
+
+            numbers = newNumbers;
+        }
+
+        if (numbers[0] == numbers[1])
+            return true;
+
+        return false;
+    }
+}
---
+++ Tests/HasSameDigitsTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Src;
+
+namespace Tests;
+
+public class HasSameDigitsTests
+{
+    [Theory]
+    [InlineData("3902", true)]
+    [InlineData("34789", false)]
+    public void Test1(string s, bool expected)
+    {
+        // Act
+        var result = HasSameDigitsSolution.HasSameDigits(s);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
