c46805c71f836f8252f5a6a37d34ef4b060acab2
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-24 08:06:46 +0200

    complete day 24-Oct-2025

---
+++ Src/NextBeautifulNumberSolution.cs
@@ -0,0 +1,39 @@
+﻿namespace Src;
+
+public class NextBeautifulNumberSolution
+{
+    public static int NumericallyBalanced(int n)
+    {
+        while (true)
+        {
+            n++;
+            if (IsNumericallyBalanced(n))
+                return n;
+        }
+    }
+
+    public static bool IsNumericallyBalanced(int x)
+    {
+        var cnt = new int[10];
+
+        var y = x;
+        while (y > 0)
+        {
+            var d = y % 10;
+
+            if (d == 0) return false;
+
+            cnt[d]++;
+
+            if (cnt[d] > d) return false;
+
+            y /= 10;
+        }
+
+        for (var d = 1; d <= 9; d++)
+            if (cnt[d] > 0 && cnt[d] != d)
+                return false;
+
+        return true;
+    }
+}
---
+++ Tests/NextBeautifulNumberTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Src;
+
+namespace Tests;
+
+public class NextBeautifulNumberTests
+{
+    [Theory]
+    [InlineData(1,22)]
+    [InlineData(1000,1333)]
+    [InlineData(3000,3133)]
+    public void Test1(int n, int expected)
+    {
+        // Act
+        var result = NextBeautifulNumberSolution.NumericallyBalanced(n);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
