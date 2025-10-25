20d9ab44040aac19680a3ac071698c343a42ae2a
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-25 07:00:56 +0200

    complete day 25-Oct-2025

---
+++ Src/CalculateMoneyinLeetcodeBankSolution.cs
@@ -0,0 +1,30 @@
+﻿namespace Src;
+
+public class CalculateMoneyinLeetcodeBankSolution
+{
+    public static int TotalMoney(int n)
+    {
+        var times = n / 7;
+        var reminder = n % 7;
+
+        var list = new List<int>();
+
+        for (var i = 0; i < times; i++)
+            list.Add(7+i);
+
+        list.Add(reminder+times);
+
+        var totalMoney = 0;
+
+        var k = 1;
+        foreach (var item in list)
+        {
+            for (var j = k; j <= item; j++)
+                totalMoney += j;
+
+            k++;
+        }
+
+        return totalMoney;
+    }
+}
---
+++ Tests/CalculateMoneyinLeetcodeBankTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Src;
+
+namespace Tests;
+
+public class CalculateMoneyinLeetcodeBankTests
+{
+    [Theory]
+    [InlineData(4, 10)]
+    [InlineData(10, 37)]
+    [InlineData(20, 96)]
+    public void Test(int days, int expected)
+    {
+        // Act
+        var result = CalculateMoneyinLeetcodeBankSolution.TotalMoney(days);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
