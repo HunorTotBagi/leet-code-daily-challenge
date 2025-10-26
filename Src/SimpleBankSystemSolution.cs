6071b02894d10c9f8e5d29539c0ca7e22954b842
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-26 07:07:07 +0100

    complete day 26-Oct-2025

---
+++ Src/SimpleBankSystemSolution.cs
@@ -0,0 +1,55 @@
+﻿namespace Src;
+
+public class SimpleBankSystemSolution
+{
+    private readonly long[] _balance;
+
+    public SimpleBankSystemSolution(long[] balance)
+    {
+        _balance = balance;
+    }
+
+    public bool Transfer(int account1, int account2, long money)
+    {
+        if (!IsValid(account1) || !IsValid(account2))
+            return false;
+
+        var i = account1 - 1;
+        var j = account2 - 1;
+
+        if (_balance[i] < money)
+            return false;
+
+        _balance[i] -= money;
+        _balance[j] += money;
+
+        return true;
+    }
+
+    public bool Deposit(int account, long money)
+    {
+        if (!IsValid(account))
+            return false;
+
+        _balance[account - 1] += money;
+
+        return true;
+    }
+
+    public bool Withdraw(int account, long money)
+    {
+        if (!IsValid(account))
+            return false;
+
+        var i = account - 1;
+
+        if (_balance[i] < money)
+            return false;
+
+        _balance[i] -= money;
+
+        return true;
+    }
+
+    private bool IsValid(int account) => account >= 1 && account <= _balance.Length;
+}
