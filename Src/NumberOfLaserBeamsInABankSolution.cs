b32ff1d49b92aa5a9e00e69f3583b559e5a57a50
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-27 10:52:38 +0100

    feat: complete day 27-Oct-2025

---
+++ Src/NumberOfLaserBeamsInABankSolution.cs
@@ -0,0 +1,26 @@
+﻿namespace Src;
+
+public class NumberOfLaserBeamsInABankSolution
+{
+    public static int NumberOfBeams(string[] bank)
+    {
+        var result = 0;
+        var prevRowDevices = 0;
+
+        foreach (var row in bank)
+        {
+            var devices = 0;
+            foreach (var c in row)
+                if (c == '1')
+                    devices++;
+            
+            if (devices == 0)
+                continue;
+
+            result += prevRowDevices * devices;
+            prevRowDevices = devices;
+        }
+
+        return result;
+    }
+}
---
+++ Tests/NumberOfLaserBeamsInABankTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Src;
+
+namespace Tests;
+
+public class NumberOfLaserBeamsInABankTests
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var input = new string[] { "011001", "000000", "010100", "001000" };
+        var expected = 8;
+
+        // Act
+        var result = NumberOfLaserBeamsInABankSolution.NumberOfBeams(input);
+
+        // Assert
+        result.Should().Be(expected);
+
+    }
+}
