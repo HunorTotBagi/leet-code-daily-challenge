a6f68422d514f716d136dd2ddf71b3ee17b5efe5
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-28 07:14:30 +0100

    feat: complete day 28-Oct-2025

---
+++ Src/MakeArrayElementsEqualToZero.cs
@@ -0,0 +1,57 @@
+﻿namespace Src;
+
+public class MakeArrayElementsEqualToZero
+{
+    public static int CountValidSelections(int[] nums)
+    {
+        var possibleDirection = new[] { "left", "right" };
+        var result = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0)
+                foreach (var direction in possibleDirection)
+                    if (ApplyMoves(nums, i, direction))
+                       result++;
+        }
+
+        return result;
+    }
+
+    public static bool ApplyMoves(int[] inputArray, int position, string direction)
+    {
+        var curr = position;
+        var nums = (int[])inputArray.Clone();
+
+        while (0 <= curr && curr < nums.Length)
+        {
+            if (nums[curr] == 0)
+            {
+                if (direction == "right")
+                    curr++;
+                else if (direction == "left")
+                    curr--;
+            }
+
+            else if (nums[curr] > 0)
+            {
+                nums[curr] -= 1;
+
+                if (direction == "right")
+                {
+                    direction = "left";
+                    curr--;
+                }
+
+                else if (direction == "left")
+                {
+                    direction = "right";
+                    curr++;
+                }
+                    
+            }
+        }
+
+        return nums.Sum() == 0;
+    }
+}
---
+++ Tests/MakeArrayElementsEqualToZeroTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Src;
+
+namespace Tests;
+
+public class MakeArrayElementsEqualToZeroTests
+{
+    [Fact]
+    public void ApplyMovesTest1()
+    {
+        // Arrange
+        var nums = new[] { 1, 0, 2, 0, 3 };
+        var position = 3;
+        var direction = "left";
+        var expected = true;
+
+        // Act
+        var result = MakeArrayElementsEqualToZero.ApplyMoves(nums, position, direction);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void ApplyMovesTest2()
+    {
+        // Arrange
+        var nums = new[] { 1, 0, 2, 0, 3 };
+        var position = 3;
+        var direction = "right";
+        var expected = true;
+
+        // Act
+        var result = MakeArrayElementsEqualToZero.ApplyMoves(nums, position, direction);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void CountValidSelectionsTest()
+    {
+        // Arrange
+        var nums = new[] { 1, 0, 2, 0, 3 };
+        var expected = 2;
+
+        // Act
+        var result = MakeArrayElementsEqualToZero.CountValidSelections(nums);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
