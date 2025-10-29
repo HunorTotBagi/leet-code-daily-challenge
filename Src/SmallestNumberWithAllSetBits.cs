6461f62f6dcbe42695cf6cf4ba954a7e9ed8c1a9
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-29 06:29:50 +0100

    feat: complete day 29-Oct-2025

---
+++ Src/SmallestNumberWithAllSetBits.cs
@@ -0,0 +1,28 @@
+﻿namespace Src;
+
+public class SmallestNumberWithAllSetBits
+{
+    public static int SmallestNumber(int n)
+    {
+        var ones = "1";
+
+        while (ConvertFromBinaryToNumber(ones) < n)
+            ones += "1";
+
+        return ConvertFromBinaryToNumber(ones);
+    }
+
+    public static int ConvertFromBinaryToNumber(string input)
+    {
+        var result = 0;
+        var length = input.Length;
+        foreach (var c in input)
+        {
+            result += (int)Math.Pow(2, length-1);
+            length--;
+        }
+
+        return result;
+    }
+}
+
---
+++ Tests/SmallestNumberWithAllSetBitsTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Src;
+
+namespace Tests;
+
+public class SmallestNumberWithAllSetBitsTests
+{
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("11", 3)]
+    [InlineData("111", 7)]
+    [InlineData("1111", 15)]
+    public void ConvertFromBinaryToNumberTest(string input, int expected)
+    {
+        // Act
+        var result = SmallestNumberWithAllSetBits.ConvertFromBinaryToNumber(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(5, 7)]
+    [InlineData(10, 15)]
+    [InlineData(3, 3)]
+    public void SmallestNumberTest(int input, int expected)
+    {
+        // Act
+        var result = SmallestNumberWithAllSetBits.SmallestNumber(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
