75706ec73ca9cfdcf3df1b0fb0e5b6a65b67251f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-30 07:11:44 +0100

    feat: complete day 30-Oct-2025

---
+++ Src/MinNumberOperationsSolution.cs
@@ -0,0 +1,17 @@
+﻿namespace Src;
+
+public class MinNumberOperationsSolution
+{
+    public static int MinNumberOperations(int[] target)
+    {
+        var operations = target[0];
+        for (var i = 1; i < target.Length; i++)
+        {
+            var difference = target[i] - target[i - 1];
+            if (difference > 0)
+                operations += difference;
+        }
+
+        return operations; 
+    }
+}
---
+++ Tests/MinNumberOperationsSolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Src;
+
+namespace Tests;
+
+public class MinNumberOperationsSolutionTests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3, 2, 1 }, 3)]
+    [InlineData(new[] { 3, 1, 1, 2 }, 4)]
+    [InlineData(new[] { 3, 1, 5, 4, 2 }, 7)]
+    public void Test1(int[] input, int expected)
+    {
+        // Act
+        var result = MinNumberOperationsSolution.MinNumberOperations(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
