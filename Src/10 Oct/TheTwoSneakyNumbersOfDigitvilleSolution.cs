d909f3e460f8cbb4e54dcda92b341157796bf151
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-31 07:56:17 +0100

    feat: complete day 31-Oct-2025

---
+++ Src/10 Oct/TheTwoSneakyNumbersOfDigitvilleSolution.cs
@@ -0,0 +1,31 @@
+﻿namespace Src._10_Oct;
+
+public class TheTwoSneakyNumbersOfDigitvilleSolution
+{
+    public static int[] GetSneakyNumbers(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+
+        foreach (var number in nums)
+        {
+            if (dict.ContainsKey(number))
+                dict[number]++;
+
+            else
+                dict[number] = 1;
+        }
+
+        var i = 0;
+        var result = new int[2];
+        foreach (var x in dict.Keys)
+        {
+            if (dict[x] == 2)
+            {
+                result[i] = x;
+                i++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Tests/10 Oct/TheTwoSneakyNumbersOfDigitvilleTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Src._10_Oct;
+
+namespace Tests._10_Oct;
+
+public class TheTwoSneakyNumbersOfDigitvilleTests
+{
+    [Theory]
+    [InlineData(new[] { 0, 1, 1, 0 }, new[] {0, 1})]
+    [InlineData(new[] { 7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2 }, new[] {4, 5})]
+    public void Test1(int[] input, int[] expected)
+    {
+        // Act
+        var result = TheTwoSneakyNumbersOfDigitvilleSolution.GetSneakyNumbers(input);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+}
